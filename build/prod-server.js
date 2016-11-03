e08cbee75e3de1c76dfc7b2e063837cac7c27e5f
dswwsd <dswwsd@Sven-rMBP.local>
2016-11-03 21:13:22 +0800

    fix static resource router

--- build/prod-server.js
+++ build/prod-server.js
@@ -1,5 +1,5 @@
 var express = require('express')
-var port = process.env.PORT || 80 // Might have some issues running it from your local machine. Best to run this on server
+var port = process.env.PORT || 8080 // Might have some issues running it from your local machine. Best to run this on server
 var baseConfig = require('./webpack.base.conf')
 
 var app = express(),
@@ -11,8 +11,7 @@ app.use(require('connect-history-api-fallback')())
 
 // server webpack outputs
 app.set('appPath', path)
-app.use(express.static(baseConfig.output.path))
-
+app.use('/static', express.static(baseConfig.output.path))
 app.route('/*')
   .get(function(req, res){
     res.sendFile(app.get('appPath') + '/index.html')
